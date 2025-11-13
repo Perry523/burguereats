5dd703440cb03aa56320494eaf10a6344dd0cb51
perry523 <wendel.renato@gmail.com>
2025-11-13 19:38:18 -0300

    teste

--- utils/convertToKebabCase.ts
+++ utils/convertToKebabCase.ts
@@ -1,7 +1,9 @@
-export function convertToKebabCase<T>(obj: Record<string, T>): Record<string, T> {
+export function convertToKebabCase<T>(
+  obj: Record<string, T>
+): Record<string, T> {
   const convertedObj = {} as Record<string, T>;
   for (const key in obj) {
-    if (obj.hasOwnProperty(key)) {
+    if (obj?.hasOwnProperty(key)) {
       const kebabKey = key.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
       convertedObj[kebabKey] = obj[key];
     }
